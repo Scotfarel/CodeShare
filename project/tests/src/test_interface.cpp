ad47d612a0430457211f1b10f61763cce76ffb4b
Ivan Zakharov <scotfarel@gmail.com>
2020-04-15 10:24:54 +0300

    Added tests skeleton

---
+++ project/tests/src/test_interface.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ivan on 4/15/20.
+//
+
+#include "gtest/gtest.h"
+#include <cstdlib>
+
+#include "ChatRoom.h"
+#include "MongoConnector.h"
+#include "RoomManager.h"
+
+
+using namespace std;
+
+TEST(Initialization, InitCorrect) {
+    EXPECT_TRUE(true);
+}
