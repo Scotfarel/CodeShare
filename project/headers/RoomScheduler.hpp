14e4a8efbc87836b5c733689e10a7f67f5a1691d
aanufriev <anufriev.artem.mail@gmail.com>
2020-04-17 16:09:29 +0300

    Add empty class RoomScheduler

---
+++ project/headers/RoomScheduler.hpp
@@ -0,0 +1,16 @@
+#ifndef PROJECT_INCLUDE_ROOM_SCHEDULER_H_
+#define PROJECT_INCLUDE_ROOM_SCHEDULER_H_
+
+#include <iostream>
+#include <vector>
+
+class RoomScheduler {
+public:
+    RoomScheduler();
+    void createRoom();
+    void getRoom();
+    void connectToRoom();
+    void deleteRoom();
+};
+
+#endif  //  PROJECT_INCLUDE_ROOM_SCHEDULER_H_
---
+++ project/src/RoomScheduler.cpp
@@ -0,0 +1,11 @@
+#include "RoomScheduler.hpp"
+
+RoomScheduler::RoomScheduler() = default;
+
+void RoomScheduler::createRoom() {}
+
+void RoomScheduler::getRoom() {}
+
+void RoomScheduler::connectToRoom() {}
+
+void RoomScheduler::deleteRoom() {}
