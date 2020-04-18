7aa9d25614ba628267146978eb995aefc7fd33eb
Ivan Zakharov <scotfarel@gmail.com>
2020-04-18 17:02:35 +0300

    Added mock for mongo conn

--- project/tests/src/test_classes_intefaces.cpp
+++ project/tests/src/test_classes_intefaces.cpp
@@ -18,13 +18,14 @@ using ::testing::SetArgReferee;
 
 using namespace std;
 
-class MockChatRoom : public ChatRoom<RoomTextBuilder> {
+class MockChatRoom : public ChatRoom<RoomTextBuilder, MongoConnector> {
 public:
     MockChatRoom()= default;
     MOCK_METHOD0(addUser, std::string());
     MOCK_METHOD0(getText, std::string());
     MOCK_METHOD0(createDbTask, bool());
 };
+
 
 // Mock-tests for ChatRoom and RoomScheduler
 TEST(MockTest, RoomSchedulerTest) {
@@ -41,20 +42,40 @@ public:
     MOCK_METHOD0(getString, std::string());
 };
 
+
 // Mock-tests for RoomTextBuilder and ChatRoom
 TEST(MockTest, TextBuilderTest) {
     MockRoomTextScheduler manager;
     EXPECT_CALL(manager, getString()).Times(AtLeast(1));
 
-    ChatRoom<MockRoomTextScheduler> room;
+    ChatRoom<MockRoomTextScheduler, MongoConnector> room;
     room.getText(&manager);
 }
 
 class MockMongoConnector : public MongoConnector {
 public:
-    MockMongoConnector() = default;
+    MockMongoConnector(std::string db, std::string coll) : dbName(std::move(db)), collName(std::move(coll)) {};
     MOCK_METHOD0(getChunk, std::string());
     MOCK_METHOD0(getCurrText, std::string());
     MOCK_METHOD0(saveDiff, bool());
-    MOCK_METHOD0(createRoomTable, bool());
+    MOCK_METHOD1(createRoomTable, bool(std::string tableName));
+private:
+    std::string dbName;
+    std::string collName;
 };
+
+
+// Mock-test for MongoDB connection to fake connector's methods
+TEST(MockTest, MongoConnectorTest) {
+    std::string name = "testName";
+    std::string table = "testTableName";
+    MockMongoConnector dbConn(name, table);
+
+    EXPECT_CALL(dbConn, getChunk()).Times(AtLeast(1));
+    EXPECT_CALL(dbConn, getCurrText()).Times(AtLeast(1));
+    EXPECT_CALL(dbConn, saveDiff()).Times(AtLeast(1));
+    EXPECT_CALL(dbConn, createRoomTable(table)).Times(AtLeast(1));
+
+    ChatRoom<MockRoomTextScheduler, MockMongoConnector> room(&dbConn);
+    room.addUser();
+}
