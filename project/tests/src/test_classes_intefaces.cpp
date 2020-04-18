fb963c6c9977c6d337532b0cbc789634ac23f658
Ivan Zakharov <scotfarel@gmail.com>
2020-04-18 17:00:59 +0300

    Updated

--- project/tests/src/test_classes_intefaces.cpp
+++ project/tests/src/test_classes_intefaces.cpp
@@ -8,9 +8,8 @@
 
 #include "ChatRoom.h"
 #include "MongoConnector.h"
-#include "RoomManager.h"
-#include "RoomTextManager.h"
-#include "TextManger.h"
+#include "RoomScheduler.h"
+#include "RoomTextBuilder.h"
 
 using ::testing::AtLeast;
 using ::testing::DoAll;
@@ -19,7 +18,7 @@ using ::testing::SetArgReferee;
 
 using namespace std;
 
-class MockChatRoom : public ChatRoom<RoomTextManager> {
+class MockChatRoom : public ChatRoom<RoomTextBuilder> {
 public:
     MockChatRoom()= default;
     MOCK_METHOD0(addUser, std::string());
@@ -27,25 +26,35 @@ public:
     MOCK_METHOD0(createDbTask, bool());
 };
 
-TEST(MockTest, RoomManagerTest) {
+// Mock-tests for ChatRoom and RoomScheduler
+TEST(MockTest, RoomSchedulerTest) {
     MockChatRoom room;
     EXPECT_CALL(room, addUser()).Times(AtLeast(1));
 
-    RoomManager<MockChatRoom> manager;
+    RoomScheduler<MockChatRoom> manager;
     manager.connectToRoom(&room);
 }
 
-class MockRoomTextManager : public RoomTextManager {
+class MockRoomTextScheduler : public RoomTextBuilder {
 public:
-    MockRoomTextManager()= default;
+    MockRoomTextScheduler()= default;
     MOCK_METHOD0(getString, std::string());
 };
 
-TEST(MockTest, TextManagerTest) {
-    MockRoomTextManager manager;
+// Mock-tests for RoomTextBuilder and ChatRoom
+TEST(MockTest, TextBuilderTest) {
+    MockRoomTextScheduler manager;
     EXPECT_CALL(manager, getString()).Times(AtLeast(1));
 
-    ChatRoom<MockRoomTextManager> room;
+    ChatRoom<MockRoomTextScheduler> room;
     room.getText(&manager);
 }
 
+class MockMongoConnector : public MongoConnector {
+public:
+    MockMongoConnector() = default;
+    MOCK_METHOD0(getChunk, std::string());
+    MOCK_METHOD0(getCurrText, std::string());
+    MOCK_METHOD0(saveDiff, bool());
+    MOCK_METHOD0(createRoomTable, bool());
+};
