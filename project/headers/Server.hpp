0473abcc1178aad7c9acbcf599ab4eba0a28890a
aanufriev <anufriev.artem.mail@gmail.com>
2020-05-17 20:19:29 +0300

    Add properties to Server

--- project/headers/Server.hpp
+++ project/headers/Server.hpp
@@ -3,22 +3,38 @@
 
 #include <iostream>
 #include "Acceptor.hpp"
+#include "Connection.hpp"
+#include "RequestHandler.hpp"
+#include "RoomScheduler.hpp"
 
 template<class AcceptorType>
 class Server {
 public:
-    Server(boost::asio::io_service* ioService, AcceptorType* acceptor) : ioService(ioService), acceptor_(acceptor) {}
+    Server(boost::asio::io_service* ioService, AcceptorType* acceptor) : 
+        ioService(ioService),
+        acceptor_(acceptor),
+        roomScheduler(),
+        newConnection(),
+        requestHandler(&roomScheduler) {}
 
     void run() {
-        acceptor_->setOption(true);
         acceptor_->open();
+        acceptor_->setOption(true);
         acceptor_->listen();
 
         ioService->run();
     }
+
+    void startAccept() {
+        newConnection.reset(new Connection<RequestHandler<RoomScheduler> >(&requestHandler));
+    }
+
 private:
     boost::asio::io_service* ioService;
     AcceptorType* acceptor_;
+    RoomScheduler roomScheduler;
+    std::shared_ptr<Connection<RequestHandler<RoomScheduler> > > newConnection;
+    RequestHandler<RoomScheduler> requestHandler;
 };
 
 #endif  //  PROJECT_INCLUDE_SERVER_H_
