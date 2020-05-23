78a562349162bb07cec1e7f08ffe5f5da68030af
aanufriev <anufriev.artem.mail@gmail.com>
2020-05-23 20:17:03 +0300

    Check method type and return json in response

--- project/headers/Connection.hpp
+++ project/headers/Connection.hpp
@@ -45,6 +45,7 @@ private:
         if (!error) {
             // boost::asio::async_write(socket_, boost::asio::buffer(data_, bytes_transferred),
             //     boost::bind(&Connection::handle_write, this,boost::asio::placeholders::error));
+
             boost::system::error_code ec;
             boost::beast::http::request_parser<boost::beast::http::string_body> parser;
             parser.eager(true);
@@ -53,11 +54,21 @@ private:
             boost::beast::http::request<boost::beast::http::string_body> request = parser.get();
             auto body = request.body();
 
-            std::stringstream jsonEncoded(body);
-            boost::property_tree::ptree root;
-            boost::property_tree::read_json(jsonEncoded, root);
+            if (request.method() == boost::beast::http::verb::post) {
+                std::stringstream jsonEncoded(body);
+                boost::property_tree::ptree root;
+                boost::property_tree::read_json(jsonEncoded, root);
 
-            std::cout << root.get<std::string>("value") << std::endl;
+                std::cout << root.get<std::string>("value") << std::endl;
+            }
+
+            boost::property_tree::ptree responseData;
+            responseData.put("roomId", 1);
+            std::stringstream response;
+            boost::property_tree::write_json(response, responseData);
+
+            boost::asio::async_write(socket_, boost::asio::buffer(response.str()),
+                boost::bind(&Connection::handle_write, this,boost::asio::placeholders::error));
         }
     }
 
