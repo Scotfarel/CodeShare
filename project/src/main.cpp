f6df40f4c9893c9320e55337e8001c8cbbcfbcdf
aanufriev <anufriev.artem.mail@gmail.com>
2020-05-25 21:11:50 +0300

    Add server start to main

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -3,7 +3,20 @@
 //
 
 #include <iostream>
+#include "boost/asio.hpp"
+
+#include "Acceptor.hpp"
+#include "Server.hpp"
 
 int main() {
-    std::cout << "Welcome to CodeShare project!" << std::endl;
+    int port = 6666;
+    boost::asio::io_service ioService;
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
+
+    Acceptor acceptor(ioService, endpoint);
+
+    Server<Acceptor> server(ioService, &acceptor);
+    server.run(port);
+    server.startAccept();
+    ioService.run();
 }
